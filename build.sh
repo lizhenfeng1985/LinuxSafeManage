da1e2d15e91c2b0eb1fd83d80ba289c9a6946911
stefanli <stefanli@tcl.com>
2017-03-16 16:03:02 +0800

    add:build.sh for build install pkg,  depend on pyinstall v2.1

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DST_DIR=SafeManage
+DST_SERVICE=SafeManageService
+DST_UI=SafeManageUI
+
+# Service
+mkdir -p $DST_DIR
+mkdir -p $DST_DIR/db
+mkdir -p $DST_DIR/license
+mkdir -p $DST_DIR/log
+
+# compile
+cd Service
+/usr/local/go/bin/go build
+cd ..
+
+cp Service/license/pub.key $DST_DIR/license/pub.key
+cp Service/server.key      $DST_DIR/server.key
+cp Service/server.crt      $DST_DIR/server.crt
+mv Service/Service         $DST_DIR/$DST_SERVICE
+
+
+# ui
+
+# compile
+cp -r UI /tmp/UI
+chown -R lzf.lzf /tmp/UI
+su lzf <<EOF
+cd /tmp/UI;
+pyinstaller -F -w gui.py
+exit;
+EOF
+
+cp /tmp/UI/dist/gui   $DST_DIR/$DST_UI 
+cp -r UI/static       $DST_DIR/
+cp -r UI/html         $DST_DIR/
+cp -r UI/config.ini   $DST_DIR/config.ini
+
+tar -zcf $DST_DIR.tar.gz $DST_DIR
+
+# clean dir
+rm -rf /tmp/UI 
